7b9c2351d4a4f367b09e302a26f0c61c8adab467
EduardaNobuntu <eduarda@nobuntu.com.br>
2025-02-18 22:07:22 -0300

    criando perfil e carrinho

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter, Routes } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { SingupUserComponent } from './user/singup-user/singup-user.component';
 import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { CarrinhoComponent } from './item/carrinho/carrinho.component';
+import { PerfilComponent } from './user/perfil/perfil.component';
 
 const routes: Routes = [
   { path: 'singUp', component: SingupUserComponent },
   { path: 'edit/:id', component: EditUserComponent },
+  { path: 'perfil/:id', component: PerfilComponent },
+  { path: 'carrinho', component: CarrinhoComponent },
 
 ];
 
