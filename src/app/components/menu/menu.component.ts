1575ccca30a300b110c454397eb36752598e3916
JMielli <dev.mielli@gmail.com>
2025-02-20 17:33:32 -0300

    Ajustes no layout do cardapio

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,7 @@
 import { Component, Input } from '@angular/core';
-
-export interface MenuItem {
-  id: number;
-  nome: string;
-  img: string;
-  preco: number;
-  descricao: string;
-}
+import { Item } from '../../item/item.model';
+import { CarrinhoComponent } from '../../item/carrinho/carrinho.component';
+import { ItemService } from '../../item/item.service';
 
 @Component({
   selector: 'app-menu',
@@ -14,52 +9,56 @@ export interface MenuItem {
   imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
+  providers: [ItemService]
+  
 })
 export class MenuComponent {
-  @Input() menuItems: MenuItem[] = [
+  @Input() menuItems: Item[] = [
     {
-      id: 1,
+      codigo: 1,
       nome: 'Strogonoff',
       preco: 29.9,
-      img: 'https://media.istockphoto.com/id/2086652998/pt/foto/chicken-stroganoff-with-rice-and-straw-potatoes-strogonoff.jpg?s=1024x1024&w=is&k=20&c=k8hk5vUDuDoo74uBmeNPUoatpqxvJ3TRM9gq5cikMSU=',
+      imagem: 'https://media.istockphoto.com/id/2086652998/pt/foto/chicken-stroganoff-with-rice-and-straw-potatoes-strogonoff.jpg?s=1024x1024&w=is&k=20&c=k8hk5vUDuDoo74uBmeNPUoatpqxvJ3TRM9gq5cikMSU=',
       descricao: 'Descrição do Prato 1',
     },
     {
-      id: 2,
+      codigo: 2,
       nome: 'Arroz & Feijão + Proteína e Salada',
       preco: 29.9,
-      img: 'https://media.istockphoto.com/id/1303029433/pt/foto/brazilian-typical-meal.jpg?s=1024x1024&w=is&k=20&c=CHv2vKvVYq7HNG_C_sH_VZl-n4mYJus_FK7fNqvM_ok=',
+      imagem: 'https://media.istockphoto.com/id/1303029433/pt/foto/brazilian-typical-meal.jpg?s=1024x1024&w=is&k=20&c=CHv2vKvVYq7HNG_C_sH_VZl-n4mYJus_FK7fNqvM_ok=',
       descricao: 'Descrição do Prato 2',
     },
     {
-      id: 3,
+      codigo: 3,
       nome: 'Feijoada Completa',
       preco: 39.9,
-      img: 'https://foodandroad.com/wp-content/uploads/2021/04/feijoada-brasil-2.jpg',
+      imagem: 'https://foodandroad.com/wp-content/uploads/2021/04/feijoada-brasil-2.jpg',
       descricao: 'Descrição do Prato 3',
     },
     {
-      id: 4,
+      codigo: 4,
       nome: 'Yakisoba Misto',
       preco: 39.9,
-      img: 'https://painel.lojavirus.com.br/uploads/comida_6_58aab0de41.png',
+      imagem: 'https://painel.lojavirus.com.br/uploads/comida_6_58aab0de41.png',
       descricao: 'Descrição do Prato 4',
     },
     {
-      id: 5,
+      codigo: 5,
       nome: 'Costelinha Barbecue',
       preco: 29.9,
-      img: 'https://melhoresmomentosdavida.com/wp-content/uploads/2022/07/pratos-tipicos-dos-estados-unidos-5.jpg',
+      imagem: 'https://melhoresmomentosdavida.com/wp-content/uploads/2022/07/pratos-tipicos-dos-estados-unidos-5.jpg',
       descricao: 'Descrição do Prato 5',
     },
     {
-      id: 6,
+      codigo: 6,
       nome: 'Filé de frango a parmegiana',
       preco: 29.9,
-      img: 'https://braseirorestaurante.com.br/wp-content/uploads/2020/07/Braseiro-476-min-scaled.jpg',
+      imagem: 'https://braseirorestaurante.com.br/wp-content/uploads/2020/07/Braseiro-476-min-scaled.jpg',
       descricao: 'Descrição do Prato 6',
     },
   ];
 
-  addToCart() {}
+  addToCart() {
+    Item.fromJson(Item).carrinho = true
+  }
 }
